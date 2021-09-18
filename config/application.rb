c9e9c8f290823464dd77613f40920f41535b3f23
mug913 <brad.alexander913@gmail.com>
2021-09-18 14:55:42 -0700

    DB Init and added User model

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,17 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module LIfeGrindBe
+module LifeGrindBe
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
+      end
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
