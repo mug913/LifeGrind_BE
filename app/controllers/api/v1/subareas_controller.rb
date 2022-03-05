5e8ddb495f65785aea899b40b11e9cb72e437323
mug913 <brad.alexander913@gmail.com>
2022-03-04 18:14:39 -0800

    implemented subarea#create route

--- app/controllers/api/v1/subareas_controller.rb
+++ app/controllers/api/v1/subareas_controller.rb
@@ -8,14 +8,17 @@ class Api::V1::SubareasController < ApplicationController
 
     def create
         #limit of 6 on backend precautionary, should be prohibited by UI design on front end. 
-        if @area.subareas.length < 7
-            @area.subareas.create(name: "", position: (@area.subareas.length), streak: 0, level: 0)
+        area = Area.find_by_id(params[:area])
+        if area.user.id == @user.id
+            if area.position == 0 && area.subareas.length < 7
+                area.subareas.create(name: params[:options][:name], position: (area.subareas.length), streak: 0, level: 0)
+                render json: {data: area.subareas, status: 422}
+            end
         end
-            render json: {areas: @user.areas, status: 422}
     end
 
     def user_params
-        params.require(:subarea).permit(:name)
+        params.require(:subarea).permit(:area, options: [:name])
     end
 
 end
